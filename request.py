f5aba880dab8c76dcca10aacff5deb3485062527
韦康琦 <3271045267@qq.com>
2023-12-08 21:14:12 +0800

    change: request.py

--- request.py
+++ request.py
@@ -1,5 +1,6 @@
 from lxml import etree
 import requests
+import sqlite3
 url = "https://gra.csu.edu.cn/yjsytz.htm"
 
 def get_html(url):
@@ -7,7 +8,12 @@ def get_html(url):
     req = requests.get(url)
     req.encoding = 'utf-8'
     tree = etree.HTML(req.text) 
-    return tree.xpath('/html/body/section/div[2]/ul/li[1]/div/a')[0].text , title + tree.xpath('/html/body/section/div[2]/ul/li[1]/div/a')[0].attrib['href']
+    news = tree.xpath('/html/body/section/div[2]/ul/li') #一页中所有的新闻
+    new_of_one_Web = len(news)
+    total = [] #所有新闻的标题,和url
+    for i in range(new_of_one_Web):
+        total.append((tree.xpath(f'/html/body/section/div[2]/ul/li[{i+1}]/div/a')[0].text,title + tree.xpath(f'/html/body/section/div[2]/ul/li[{i+1}]/div/a')[0].attrib['href']))
+    return total
 
 if __name__ == '__main__':
     print(get_html(url))
